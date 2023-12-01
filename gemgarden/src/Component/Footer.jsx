6d7dfed7b8b8d1ae1cf51b916991053cd71927a8
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-01 14:07:09 +0530

    new

--- gemgarden/src/Component/Footer.jsx
+++ gemgarden/src/Component/Footer.jsx
@@ -82,6 +82,8 @@ export default function Footer() {
     backgroundColor={"#262425"}
       // bg={useColorModeValue('gray.50', 'gray.900')}
       // color={useColorModeValue('gray.700', 'gray.200')}
+
+      
      color="#FFFFFF"
       >
       <Container as={Stack} maxW={'6xl'} py={10}>
