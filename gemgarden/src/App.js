b94e6ece067a80dc0143baf37f100e0a2d11ce6b
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-01 13:16:31 +0530

    Footer Done

--- gemgarden/src/App.js
+++ gemgarden/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { AllRoutes } from './Routing/AllRoutes';
 import { Homepage } from './Pages/Homepage';
 import Navbar from "./Component/Navbar";
+import Footer from './Component/Footer';
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Homepage/>
+      <Footer/>
     </div>
   );
 }
--- gemgarden/src/index.js
+++ gemgarden/src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <ChakraProvider>
     <BrowserRouter>
-       <App />
+    {/* <ColorModeScript> */}
+     <App />
+    {/* </ColorModeScript> */}
     </BrowserRouter>
+    </ChakraProvider>
+    
     
 );
 
