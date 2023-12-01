159a28e39bbec21a116fad2548c461ad726f5a4d
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-01 14:10:41 +0530

    Update Footer.jsx

--- gemgarden/src/Component/Footer.jsx
+++ gemgarden/src/Component/Footer.jsx
@@ -14,6 +14,7 @@ import {
   Flex
 } from '@chakra-ui/react';
 
+
 import { ReactNode } from 'react';
 import { FaInstagram, FaTwitter, FaYoutube , FaPinterest } from 'react-icons/fa';
 import { BiMailSend } from 'react-icons/bi';
